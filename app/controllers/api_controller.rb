db682269cb83dce19415d2ad65b77a9be3d78456
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-07-17 19:03:31 -0400

    moved url checking/error messages to search model for soem reason. merging into master

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,9 +2,9 @@ class ApiController < ApplicationController
 
   def analyze
 
-  id_1 = Api.extract_video_id(params[:url1])
-  id_2 = Api.extract_video_id(params[:url2])
-  return render json: Api.error_message_json(:url)  if !id_1 or !id_2
+  id_1 = Search.extract_video_id(params[:url1])
+  id_2 = Search.extract_video_id(params[:url2])
+  return render json: Search.error_message_json(:url)  if !id_1 or !id_2
   video_1_data = Api.analyze(id_1)
   video_2_data = Api.analyze(id_2)
       render json: {
--- app/models/api.rb
+++ app/models/api.rb
@@ -1,15 +1,5 @@
 class Api < ActiveRecord::Base
 
-  def self.error_message_json message
-    messages = {
-      :url => 'Invalid url(s). Try again'
-    }
-    return {
-      :success? => false,
-      :message => messages[message]
-    }
-  end
-
   def self.get_comments (video_id)
     query = {
       :query => {
@@ -75,13 +65,5 @@ class Api < ActiveRecord::Base
     return stats.merge(tone)
   end
 
-  def self.extract_video_id(url)
-    id = url.split('youtube.com/watch?v=').last
-    if id.length == url.length or url.length==0
-      return false
-    else
-      return id
-    end
-  end
 
 end
