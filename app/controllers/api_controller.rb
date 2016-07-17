06fc91da362ba7097226b9fea9b280b2d212d950
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-07-17 18:54:51 -0400

    removed log messages

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,11 +4,7 @@ class ApiController < ApplicationController
 
   id_1 = Api.extract_video_id(params[:url1])
   id_2 = Api.extract_video_id(params[:url2])
-  puts id_1, id_2
   return render json: Api.error_message_json(:url)  if !id_1 or !id_2
-
-
-
   video_1_data = Api.analyze(id_1)
   video_2_data = Api.analyze(id_2)
       render json: {
